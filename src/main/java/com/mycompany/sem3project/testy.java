0f801d2c962f8cf060df53e14360f4b9ee246760
olls <olls@HenrikDesktop>
2016-11-16 16:25:05 +0100

    startwithtravistestytest

---
+++ src/main/java/com/mycompany/sem3project/testy.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.sem3project;
+
+/**
+ *
+ * @author olls
+ */
+public class testy
+{
+
+    public testy()
+    {
+        
+    }
+    
+    public static int giveMeTen()
+    {
+        return 10;
+    }
+}
---
+++ src/test/java/testytest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.mycompany.sem3project.testy;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author olls
+ */
+public class testytest
+{
+
+    public testytest()
+    {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void testFunctionality()
+    {
+        assertEquals(10, testy.giveMeTen());
+    }
+}
