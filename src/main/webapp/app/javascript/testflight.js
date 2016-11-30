06b16dbf8f76db550380f3c0404a3ec24c815229
olls <olls@HenrikDesktop>
2016-12-01 00:08:55 +0100

    Solo programming: Henrik
    Multiple flights shows up from Lar's dummy airline. No search function,
    yet.
    
    Flights are not shown correctly in their item groups.

--- src/main/webapp/app/javascript/testflight.js
+++ src/main/webapp/app/javascript/testflight.js
@@ -9,23 +9,69 @@ var myApp = angular.module('flights', []);
 myApp.controller('testFlightCtrl', function ($scope, $http) {
 
     var self = this;
+    self.flightNr = 0;
     self.flightData = [];
-    self.dataSize = self.flightData.length + 1;
+    self.flights = [];
+    self.dataSize = 0;
     self.flightsGotten = false;
-    $scope.getFlight = function () {
+    self.items = 0;
+    self.itemArray = [0];
+    
+
+    self.getFlight = function () {
         $http.get('api/flight/testFlight')
                 .success(function (data) {
                     self.flightData = data;
                     self.flightsGotten = true;
-                    self.dataSize = self.flightData.length + 1 ;
+                    self.dataSize = self.flightData.flights.length;
+
+
+                    for (i = 0; i < self.dataSize; i++)
+                    {
+                        if (i % 3 === 0)
+                        {
+                            self.items++;
+                            
+                        }
+                    }
+                    
+                    self.itemArray = new Array(self.items);
+                    
+
+
                 }).error(function (data) {
             $scope.error = data.error + data.message;
         });
 
-    
+
 
     };
 
+    self.resetFlightNr = function ()
+    {
+        self.flightNr = 0;
+    };
+    self.incFlightNr = function ()
+    {
+        self.flightNr++;
+    };
+
+    self.showItem = function (num)
+    {
+
+        if (num % 3 === 0)
+        {
+            return true;
+        }
+
+        return false;
+    };
+
+    self.getFlightCount = function ()
+    {
+
+        return new Array(3);
+    };
 
 });
 
