b5095ec3f6ee5d586b1472bf5d243d4fdcbeaa44
olls <olls@HenrikDesktop>
2016-11-30 20:07:23 +0100

    Solo programming: Henrik
    Edited some images. 
    A signle flight from dummy airline can show up again when searche
    button is clicked.

--- src/main/webapp/app/javascript/testflight.js
+++ src/main/webapp/app/javascript/testflight.js
@@ -10,33 +10,39 @@ myApp.controller('testFlightCtrl', function ($scope, $http) {
 
     var self = this;
     self.flightData = [];
-
+    self.dataSize = self.flightData.length + 1;
+    self.flightsGotten = false;
     $scope.getFlight = function () {
         $http.get('api/flight/testFlight')
                 .success(function (data) {
                     self.flightData = data;
+                    self.flightsGotten = true;
+                    self.dataSize = self.flightData.length + 1 ;
                 }).error(function (data) {
             $scope.error = data.error + data.message;
         });
+
+    
+
     };
-    
-    
+
+
 });
 
 
 
-myApp.filter('traveltime', function() {
-        return function(input, traveltime) {
-            var h = Math.floor(input / 60); 
-            var m = (input - (h*60)); 
-            
-            if (m > 0)
+myApp.filter('traveltime', function () {
+    return function (input, traveltime) {
+        var h = Math.floor(input / 60);
+        var m = (input - (h * 60));
+
+        if (m > 0)
             var time = h + 'h ' + m + 'm';
-            else if(m === 0)
-              var time = h + 'h ';
-            return time;
-        };
-    });
+        else if (m === 0)
+            var time = h + 'h ';
+        return time;
+    };
+});
 
 
 
